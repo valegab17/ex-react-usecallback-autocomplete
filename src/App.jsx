8d909ec8d64e3c1babd3abe17afd694bf6c023df
valeriagabrielli <gabvale53@gmail.com>
2026-05-30 18:14:11 +0200

    milestone2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 import './index.css';
-import { useState, useEffect, useMemo, memo } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+
+//mi scrivo la funzione di debounceà
+function debounce(callback, delay) {
+  let timer;
+  return (value) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(value);
+    }, delay);
+  }
+}
 function App() {
   const API_URL = "http://localhost:3333";
 
   const [query, setQuery] = useState('');
   const [suggestions, setSuggestions] = useState([]);
-  console.log("Stato attuale -> Query:", query, "Suggerimenti:", suggestions);
+  //riuso la funzione di debounce
+  const debouncedFetch = useCallback(
+    debounce((text) => {
+
+      fetch(`${API_URL}/products?search=${text}`)
+        .then(res => res.json())
+        .then(data => setSuggestions(data));
+
+
+    }, 200),
+    []
+  )
+
+
   useEffect(() => {
-    fetch(`${API_URL}/products?search=${query}`)
-      .then(res => res.json())
-      .then(data => setSuggestions(data));
-  }, [query])
+    debouncedFetch(query);
+  },[query, debouncedFetch]);
+  console.log("Stato attuale -> Query:", query, "Suggerimenti:", suggestions);
+
 
   return (
     <>
@@ -28,7 +52,7 @@ function App() {
                 <li key={i.id}>{i.name}
                   <img src={i.image}
                     alt={i.name}
-                    />
+                  />
                 </li>
               ))}
             </ul>
@@ -43,3 +67,10 @@ function App() {
 
 export default App
 
+/* 📌 Milestone 2: Implementare il Debounce per Ottimizzare la Ricerca
+
+    Attualmente, ogni pressione di tasto esegue una richiesta API. Questo è inefficiente!
+    Implementa una funzione di debounce per ritardare la chiamata API fino a quando l’utente smette di digitare per un breve periodo (es. 300ms)
+    Dopo l’implementazione, verifica che la ricerca non venga eseguita immediatamente a ogni tasto premuto, ma solo dopo una breve pausa.
+
+Obiettivo: Ridurre il numero di richieste API e migliorare le prestazioni. */
